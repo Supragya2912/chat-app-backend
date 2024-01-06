f3a35c7f3325cb8c94b4dfa63e2e7d10ea3f904a
Supragya2912 <anandsupragya@gmail.com>
2024-01-06 17:57:16 +0530

    connection using socket io

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const morgan = require('morgan');
 const xss = require('xss');
 const cors = require('cors');
 const { mongo } = require('mongoose');
-const {connectToDB } = require('./db');
+const { connectToDB } = require('./db');
 const port = process.env.PORT;
 const routes = require('./routes/index')
 const authRoute = require("./routes/auth");
@@ -21,10 +21,10 @@ connectToDB();
 
 const server = http.createServer(app);  // Create an HTTP server
 
-const io = socketIo(server,{
+const io = socketIo(server, {
   cors: {
     origin: "http://localhost:3000",
-    methods:["GET", "POST"]
+    methods: ["GET", "POST"]
   }
 });
 
@@ -33,29 +33,29 @@ app.use(mongoSanitize());
 // app.use(xss());
 
 app.use(cors({
-    origin: "*",
-    methods: ['GET','PATCH', 'POST', 'PUT', 'DELETE'],
-    credentials: true
+  origin: "*",
+  methods: ['GET', 'PATCH', 'POST', 'PUT', 'DELETE'],
+  credentials: true
 }));
-app.use(express.json({limit: "10kb"}));
+app.use(express.json({ limit: "10kb" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 process.on('uncaughtException', err => {
-    console.log('UNCAUGHT EXCEPTION! Shutting down...');
-    console.log(err.name, err.message);
-    process.exit(1);
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
 });
 
 const limiter = rateLimit({
-    max: 3000,
-    windowMs: 60 * 60 * 1000,
-    message : "Limit exceeded"
+  max: 3000,
+  windowMs: 60 * 60 * 1000,
+  message: "Limit exceeded"
 })
 app.use("/tawk", limiter);
 
@@ -75,8 +75,8 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-io.on("connection", async (socket) =>{
-  
+io.on("connection", async (socket) => {
+
   console.log(socket);
 
   const user_id = socket.handshake.query["user_id"]
@@ -85,18 +85,18 @@ io.on("connection", async (socket) =>{
 
   console.log(`User connected, ${socket_id}`);
 
-  if(user_id){
-    await User.findByIdAndUpdate(user_id,{  socket_id  })
+  if (user_id) {
+    await User.findByIdAndUpdate(user_id, { socket_id })
   }
-  
-  socket.on("friend_request", async(data) =>{
+
+  socket.on("friend_request", async (data) => {
     console.log(data.to);
 
     const to = await User.findById(data.to);
 
 
-    io.to(to.socket_id).emit("new_friend_request",{
-      
+    io.to(to.socket_id).emit("new_friend_request", {
+
     });
   })
 
