f7b9dec59264a9024f2898433c797b81373fd777
Supragya2912 <anandsupragya@gmail.com>
2024-01-10 00:53:48 +0530

    updated get friends api

--- controllers/user.js
+++ controllers/user.js
@@ -48,10 +48,10 @@ exports.getFreindRequest = async (req, res, next) => {
 
 exports.getFriends = async (req, res, next) => {
 
-    const friends = await User.findById(req.user._id).populate("friends","_id firstName lastName");
+    const this_user = await User.findById(req.user._id).populate("friends","_id firstName lastName");
     res.status(200).json({
         status:"success",
-        data: friends,
+        data: this_user.friends,
         message: "Friends fetched successfully"
     })
     
